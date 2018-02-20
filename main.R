d8cce9c02c7779376f5a93d6e0ce2192c455c702
Gavin Lenton <gavin.lenton@griffithuni.edu.au>
2018-02-20 10:42:39 +1000

    Refined text output, analysed different armour types

--- main.R
+++ main.R
@@ -1,17 +1,19 @@
 ## ----- PROCESS data ----- ##
 
+# Load packages -----------------------------------------------------------
 # Load Tidyverse (for some reason it doesn't recognise tidyverse as a package so I load it individual package)
-require("ggplot2"); require("tibble"); require("tidyr"); require("readr"); require("purrr"); require("dplyr"); require("forcats"); require("tidyselect"); library("broom")
+require("ggplot2"); require("tibble"); require("tidyr"); require("readr"); require("purrr"); require("dplyr"); 
+require("forcats"); require("tidyselect"); library("broom")
 
 # Load libraries for multiple imputation by chained equations
 require("Rcpp"); require("VIM"); require("mice"); require("car")
 
-## Gives count, mean, standard deviation, standard error of the mean, and confidence interval (default 95%).
+# Functions ---------------------------------------------------------------
+## Function to give count, mean, standard deviation, standard error of the mean, and confidence interval
 ##   measurevar: the name of a column that contains the variable to be summarized
 ##   groupvars: a vector containing names of columns that contain grouping variables
 ##   conf.interval: the percent range of the confidence interval (default is 95%)
-summarySE <- function(data=NULL, measurevar, groupvars=NULL, na.rm=FALSE,
-                      conf.interval=.95, .drop=TRUE) {
+summarySE <- function(data=NULL, measurevar, groupvars=NULL, na.rm=FALSE, conf.interval=.95, .drop=TRUE) {
   library(plyr)
   
   # New version of length which can handle NA's: if na.rm==T, don't count them
@@ -87,10 +89,9 @@ missingValueImputation <- function(data2impute){
   # maxit – Refers to no. of iterations taken to impute missing values
   # method – Refers to method used in imputation. we used predictive mean matching (pmm).
   # m  – Refers to 5 imputed data sets
-  imputed_Data <- invisible(mice(data2impute, m=5, maxit = 5, method = 'pmm'));
+  imputed_Data <- mice(data2impute, m=5, maxit = 5, method = 'pmm', printFlag = FALSE);
   
   completeData <- invisible(complete(imputed_Data,1))
-  
   return(completeData);
 }
 
@@ -142,11 +143,11 @@ outlierKD <- function(dt, var) {
     dt <- dt %>% 
       select(Participants, Armour, Mass, Speed, !!name)
     
-    cat("Outliers successfully removed")
+    cat("Outliers successfully removed", "\n")
     return(dt)
     return(response)
   } else{
-    cat("Nothing changed", "n")
+    cat("Nothing changed", "\n")
     return(dt)
     return(response)
   }
@@ -180,15 +181,24 @@ outputSummary <- function(dt, varName, model_ANOVA){
   summary_armour <- summarySE(data = dt, measurevar=paste(varName, "noOut", sep = "_"), groupvars=model_ANOVA$term[1]) 
 
   # Save to .text files
-  print(summary_all); print(summary_armour_mass); print(summary_armour_speed); print(summary_mass_speed); print(summary_armour); 
-  print(summary_mass); print(summary_speed);
-  
+  cat('\n', 'COMPARISON OF ALL\n'); print(summary_all, digits = 3); cat('\n', 'ARMOUR * MASS\n'); print(summary_armour_mass, digits = 3); 
+  cat('\n', 'ARMOUR * SPEED\n'); print(summary_armour_speed, digits = 3); cat('\n', 'MASS * SPEED\n'); print(summary_mass_speed, digits = 3);
+  cat('\nARMOUR\n'); print(summary_armour, digits = 3); cat('\nMASS\n');print(summary_mass, digits = 3); 
+  cat('\nSPEED\n'); print(summary_speed, digits = 3);
 }
 
-# Define path to obtain raw data from
+# Define paths ------------------------------------------------------------
+# Define path to raw data
 armour_type_dir <- "~/Google Drive/postDoctoralWork/2018 defence contact forces/EMG-assisted results/stats/contact-forces-results/only_armour_type"
+setwd(armour_type_dir);
 fullFileNames <- dir(path = armour_type_dir, pattern="*.csv", full.names = TRUE)
 fileNames <- dir(path = armour_type_dir, pattern="*.csv")
+# Define sub-directories to save data in along the way
+subDirText = "summaries"; subDirOutliers = "outliersRemoved"; subDirQQplot = "QQ_plots";
+
+
+
+# Data processing ---------------------------------------------------------
 
 # Load data and store in data frame
 y <- 1
@@ -198,17 +208,24 @@ for(i in fullFileNames){
   t <- regexpr(".csv", fileNames[y]) # Determine where the stop index is
   varName <- substr(fileNames[y], start = 1, stop = t-1) # Use substr to extract variable name without .csv
   
-  # Read .csv file
+  # Read .csv file containing raw data
   exp.data = as_tibble(read.csv(i))
   
-  # Redirect all output to a text file.
-  sink(paste("./summaries/", varName, "_results.txt", sep = ""), append=FALSE, split=FALSE)
+  # Redirect all output to a text file - create folder if it doesn't exist already
+  if (file.exists(subDirText)){
+    setwd(file.path(armour_type_dir, subDirText))
+  } else {
+    dir.create(file.path(armour_type_dir, subDirText))
+    setwd(file.path(armour_type_dir, subDirText))
+    
+  }
+  sink(paste(file.path(armour_type_dir, subDirText), "/", varName, "_results.txt", sep = ""), append=FALSE, split=FALSE)
   
-  print("");
-  print("");
-  print("=========================== VARIABLE: ============================")
-  print(varName)
-  print("==================================================================")
+  # Jump back out to armour type folder
+  setwd(armour_type_dir);
+  
+  # Print variable name to text file
+  cat("\n=========== VARIABLE: ", varName, " =============\n\n")
   
   # Reshape to long format
   exp.data.reshaped <- reshape4ANOVA(exp.data, varName)
@@ -223,31 +240,39 @@ for(i in fullFileNames){
   # Stop outputting to text
   sink()
   
-  # Write new data to .csv
-  saveRDS(exp.data.noOut, paste("./outliersRemoved/", varName, "_noOut.rds", sep = ""))
+  # Write new data to .RDS
+  saveRDS(exp.data.outRemoved, paste(armour_type_dir, "/", varName, "_noOut.rds", sep = ""))
   
   # Output to text again
-  sink(paste("./summaries/", varName, "_results.txt", sep = ""), append=TRUE, split=FALSE)
+  sink(paste(file.path(armour_type_dir, subDirText), "/", varName, "_results.txt", sep = ""), append=TRUE, split=FALSE)
   
-  # Test for normality
-  shapiro_test <- shapiro.test(exp.data.outRemoved);
-  print(shapiro_test$p.value)
+  # Test for normality with shapiro wilk method
+  shapiro_test <- shapiro.test(exp.data.outRemoved[,length(exp.data.outRemoved)]);
+  shapiro_test$data.name <- varName; print(shapiro_test)
   sink()
   
   # QQ plots to show normality
   require(graphics)
   graphics.off()
   
-  # Save QQ plot
-  png(file= paste("./QQ_plots/QQ_plot_", varName, ".png", sep = ""),width = 7, height = 5, units = 'in', res = 300)
-  qqnorm(exp.data.outRemoved[,length(exp.data.outRemoved)-1], main = "Normal Q-Q Plot",
+  # Save QQ plot - make directory to store plots if it doesn't exist already
+  if (file.exists(subDirQQplot)){
+    setwd(file.path(armour_type_dir, subDirQQplot))
+  } else {
+    dir.create(file.path(armour_type_dir, subDirQQplot))
+    setwd(file.path(armour_type_dir, subDirQQplot))
+  }
+  
+  png(file= paste(file.path(armour_type_dir, subDirQQplot), "/QQ_plot_", varName, ".png", sep = ""),
+      width = 7, height = 5, units = 'in', res = 300)
+  qqnorm(exp.data.outRemoved[,length(exp.data.outRemoved)], main = "Normal Q-Q Plot",
          xlab = "Theoretical Quantiles", ylab = varName,
          plot.it = TRUE, datax = FALSE)
-  qqline(exp.data.outRemoved[,length(exp.data.outRemoved)-1], col = 2, probs = c(0.2, 0.8), qtype = 7)
-  graphics.off()
+  qqline(exp.data.outRemoved[,length(exp.data.outRemoved)], col = 2, probs = c(0.2, 0.8), qtype = 7)
+  graphics.off(); setwd(armour_type_dir);
   
   # Output to same text file
-  sink(paste("./summaries/", varName, "_results.txt", sep = ""), append=TRUE, split=FALSE)
+  sink(paste(file.path(armour_type_dir, subDirText), "/", varName, "_results.txt", sep = ""), append=TRUE, split=FALSE)
 
   # Assign model for the ANOVA
   aov_model_expression_as_string = paste('aov(', varName, '_noOut ', '~ Armour * Mass * Speed, data = .)', sep = "") # Armour X Mass X Speed
@@ -255,16 +280,34 @@ for(i in fullFileNames){
     anova_wrapper(model_expression_as_string = aov_model_expression_as_string, type="II") # Type 2 repeated measures ANOVA
   
   # Print to text file
-  print("ANOVA comparing armour types, mass, and walking speed")
-  print(model_ANOVA)
+  cat("ANOVA comparing armour types, mass, and walking speed\n")
+  print(model_ANOVA, digits = 3)
 
+  # Detect p-values <0.05 and perform post-hoc tests 
+  # if (model_ANOVA$p.value[1:length(model_ANOVA$p.value)-1] < 0.05) {
+  #   sigDiff <- as.tibble(model_ANOVA$p.value[1:length(model_ANOVA$p.value)-1] < 0.05)
+  #   diffIndex <- which(sigDiff$value, arr.ind = FALSE, useNames = TRUE) # Find index of sig diff
+  #   variablesWithDiff <- unlist(strsplit(model_ANOVA$term[diffIndex], split = ":")) # Get name of factors with difference
+  #   # Run t.tests for comparisons
+  #   if (length(varName < 2)) {
+  #     t.test(paste0(varName, '_noOut ', '~ ', variablesWithDiff[1]), data = ., mu = 0, paired = TRUE,
+  #            conf.level = 0.95)
+  #     } else {
+  #       t.test(paste0(varName, '_noOut ', '~ ', variablesWithDiff[1]), data = ., mu = 0, paired = TRUE,
+  #              conf.level = 0.95)
+  #       t.test(paste0(varName, '_noOut ', '~ ', variablesWithDiff[2]), data = ., mu = 0, paired = TRUE,
+  #              conf.level = 0.95)
+  #   }
+  # 
+  # }
+  
   # Generate summary statistics and print it all to the text file
   outputSummary(exp.data.outRemoved, varName, model_ANOVA)
   sink()
 
   # Create one large variable with all the data
   if (y == 1){
-    allData <- exp.data.noOut
+    allData <- exp.data.outRemoved
   } else{
     # Join with previous tibble and match participants, armour, mass, and speed
     allData <- allData %>% 
